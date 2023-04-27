3de8a5a63e44af042a074323221f5b20e7d1b45e
Tim Yang <qingchuanyang28@gmail.com>
2023-04-27 12:19:17 -0400

    modify file name

--- src/main/java/BitcoinPrices.java
+++ src/main/java/BitcoinPrices.java
@@ -1,6 +1,4 @@
-import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.MapFunction;
-import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -10,35 +8,26 @@ import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.util.Collector;
 import org.apache.flink.streaming.api.windowing.time.Time;
 
-public class SocketWindowWordCount {
+public class BitcoinPrices {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
 
-        DataStream<String> input = env.socketTextStream("localhost", 9001, "\n");
-
-//        DataStream<Tuple2<String, Integer>> parsed = input.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
-//            @Override
-//            public void flatMap(String s, Collector<Tuple2<String, Integer>> collector) throws Exception {
-//                for (String word: s.split("\\s")) {
-//                    collector.collect(Tuple2.of(word, 1));
-//                }
-//            }
-//        }).keyBy(0).timeWindow(Time.seconds(5)).sum(1);
+        DataStream<String> input = env.socketTextStream("localhost", 9000, "\n");
 
         // Parse the Bitcoin prices and extract the price field
         DataStream<Double> prices = input.map(new MapFunction<String, Double>() {
             @Override
             public Double map(String value) throws Exception {
-                return Double.parseDouble(value.split(",")[1]);
+                return Double.parseDouble(value.split("\n")[0]);
             }
         });
 
-        DataStream<Double> avgPrices = parsed.keyBy((Double price) -> 0)
+        DataStream<Double> avgPrices = prices.keyBy((Double price) -> 0)
                 .window(SlidingProcessingTimeWindows.of(Time.minutes(1), Time.seconds(1)))
                 .apply(new AveragePrice());
 
-        parsed.print();
+        avgPrices.print();
         env.execute("Socket Window WordCount");
     }
 
